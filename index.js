7aae6ad16eb61262956717bee28554eecd2aea2a
Daphne Watson <daphne.renee.laurels@gmail.com>
2022-09-12 15:46:46 -0700

    wrapped a JSOn object around the data with a status, statusText and message property

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // The express server is brought in and create the application
-let express = require('express')
+let express = require('express');
 let app = express();
 
 //Call in the Express Router object
@@ -16,7 +16,12 @@ let produce = [
 	// this function has a request object, response object, and a next object
 router.get('/', function (req, res, next) {
 	// send out the test text 'Mango' 
-	res.status(200).send(produce);
+	res.status(200).json({
+		"status": 200,
+		"statusText": "OK",
+		"message": "Farmer's market items are successully collected",
+		"data": produce
+	});
 });
 
 // Start up the router so all routers are fixed with /api/v1
