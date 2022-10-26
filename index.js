f18ddd78dc8fb12df28306115af9323ac58aa8e1
Daphne Watson <daphne.renee.laurels@gmail.com>
2022-10-25 20:58:58 -0700

    adding code for a single item lookup for success 200 or not found 404 message

--- index.js
+++ index.js
@@ -24,6 +24,34 @@ router.get('/', function (req, res, next) {
 	});
 });
 
+//New route
+// Create GET/id to return a single market item
+	router.get('/:id', function (req, res, next) {
+		marketItems.getById(req.params.id, function (data) {
+			if (data) {
+				res.status(200).json({ // a success message with status 200
+					"status": 200, 
+					"statusText": "OK",
+					"message": "Single market item retrieved.",
+					"data": data
+				});
+			} else {
+				res.status(404).json({ // an error message with status 404
+					"status": 404,
+					"statusText": "Not Found",
+					"message": "The market item '" + req.params.id + "' could not be found.",
+					"error": {
+						"code": "NOT_FOUND",
+						"message": "The market item '" + req.params.id + "' could not be found."
+					}
+				});
+			}
+		}, function(err) {
+			next(err);
+	});
+});
+
+
 // Start up the router so all routers are fixed with /api/v1
 app.use('/api/', router);
 
--- repos/marketItems.js
+++ repos/marketItems.js
@@ -11,6 +11,16 @@ let marketItems = {
 				resolve(JSON.parse(data));
 			}
 		});
+	},
+	getById: function (id, resolve, reject) {
+		fs.readFile(FILE_NAME, function (err, data) {
+			if (err) {
+				reject(err);
+			} else {
+				let item = JSON.parse(data).find(p => p.id == id);
+				resolve(item);
+			}
+		})
 	}
 };
 
